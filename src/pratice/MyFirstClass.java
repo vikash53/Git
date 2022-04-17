b6f3dd3532fa99f83e6f4bcc22e612cf056a7859
vikash53 <vikashavit27@gmail.com>
2022-04-17 11:27:14 +0530

    This is My First Java Class

---
+++ src/pratice/MyFirstClass.java
@@ -0,0 +1,11 @@
+package pratice;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("code pushe to github");
+
+	}
+
+}
