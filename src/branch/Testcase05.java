66a1a08efc6a4201f2a93c3cdb1f224ba5ceacf6
vikash53 <vikashavit27@gmail.com>
2022-04-17 14:16:26 +0530

    testcase 5 added to newbranch

---
+++ src/branch/Testcase05.java
@@ -0,0 +1,10 @@
+package branch;
+
+public class Testcase05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("keep testcase5 in new branch");
+	}
+
+}
--- src/pratice/Testcase003.java
+++ src/pratice/Testcase003.java
@@ -5,6 +5,7 @@ public class Testcase003 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("testcaseoo3 is automated ");
+System.out.println("keep this class in new branch");
 	}
 
 }
