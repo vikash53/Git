5bf4970d6acf1c592e2890acf7955e36b207c9ac
vikash53 <vikashavit27@gmail.com>
2022-04-17 12:05:35 +0530

    TestCase 2 Automated

--- src/pratice/MyFirstClass.java
+++ src/pratice/MyFirstClass.java
@@ -5,6 +5,7 @@ public class MyFirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("code pushe to github");
+		System.out.println("Adding one More Extra Line");
 
 	}
 
---
+++ src/pratice/TestCase002.java
@@ -0,0 +1,11 @@
+package pratice;
+
+public class TestCase002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testcase 002 for Automation");
+
+	}
+
+}
